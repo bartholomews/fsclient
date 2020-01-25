7d208e3557ec7aa3a26fdb2b2c36cb6de4261def
bartholomews <git@bartholomews.io>
2020-01-25 13:26:19 +0000

    `Derivations` -> withCustomKey pureconfig helper

---
+++ src/main/scala/fsclient/config/Derivations.scala
@@ -0,0 +1,19 @@
+package fsclient.config
+
+import pureconfig.{ConfigReader, Derivation}
+
+private[fsclient] object Derivations {
+
+  private def customKeyConfigReader[A](key: String)(implicit reader: ConfigReader[A]): ConfigReader[A] =
+    ConfigReader.fromCursor { cur =>
+      for {
+        objCur <- cur.asObjectCursor
+        appNameObjCursor <- objCur.atKey(key)
+        appConfigObjCursor <- appNameObjCursor.asObjectCursor
+        res <- implicitly[ConfigReader[A]].from(appConfigObjCursor)
+      } yield res
+    }
+
+  def withCustomKey[A](key: String)(implicit reader: ConfigReader[A]): Derivation[ConfigReader[A]] =
+    Derivation.Successful(customKeyConfigReader(key))
+}
