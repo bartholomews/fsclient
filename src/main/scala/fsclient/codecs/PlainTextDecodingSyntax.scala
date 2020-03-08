11e5c5dc05154841d1cf1e9ab3c8aea7ccdb596e
bartholomews <git@bartholomews.io>
2020-03-08 23:29:01 +0000

    Removed unnecessary regex pipe decoder helpers

--- src/main/scala/fsclient/codecs/PlainTextDecodingSyntax.scala
+++ src/main/scala/fsclient/codecs/PlainTextDecodingSyntax.scala
@@ -1,11 +1,9 @@
 package fsclient.codecs
 
 import cats.effect.Effect
+import cats.implicits._
 import fs2.{INothing, Pipe}
 import fsclient.entities.EmptyResponseException
-import cats.implicits._
-
-import scala.util.matching.Regex
 
 trait PlainTextDecodingSyntax {
 
@@ -25,42 +23,4 @@ trait PlainTextDecodingSyntax {
       .flatMap(pf.andThen(entity => fs2.Stream.emit(entity)).orElse {
         case other => raiseError[F](other)
       })
-
-  final def plainTextRegexDecoderPipe1[F[_]: Effect, A](reg: Regex)(fn: String => A): Pipe[F, String, A] =
-    plainTextDecoderPipe({
-      case Right(reg(a)) => fn(a)
-    })
-
-  final def plainTextRegexDecoderPipe2[F[_]: Effect, A](reg: Regex)(fn: (String, String) => A): Pipe[F, String, A] =
-    plainTextDecoderPipe({
-      case Right(reg(a, b)) => fn(a, b)
-    })
-
-  final def plainTextRegexDecoderPipe3[F[_]: Effect, A](
-    reg: Regex
-  )(fn: (String, String, String) => A): Pipe[F, String, A] =
-    plainTextDecoderPipe({
-      case Right(reg(a, b, c)) => fn(a, b, c)
-    })
-
-  final def plainTextRegexDecoderPipe4[F[_]: Effect, A](
-    reg: Regex
-  )(fn: (String, String, String, String) => A): Pipe[F, String, A] =
-    plainTextDecoderPipe({
-      case Right(reg(a, b, c, d)) => fn(a, b, c, d)
-    })
-
-  final def plainTextRegexDecoderPipe5[F[_]: Effect, A](
-    reg: Regex
-  )(fn: (String, String, String, String, String) => A): Pipe[F, String, A] =
-    plainTextDecoderPipe({
-      case Right(reg(a, b, c, d, e)) => fn(a, b, c, d, e)
-    })
-
-  final def plainTextRegexDecoderPipe6[F[_]: Effect, A](
-    reg: Regex
-  )(fn: (String, String, String, String, String, String) => A): Pipe[F, String, A] =
-    plainTextDecoderPipe({
-      case Right(reg(a, b, c, d, e, f)) => fn(a, b, c, d, e, f)
-    })
 }
