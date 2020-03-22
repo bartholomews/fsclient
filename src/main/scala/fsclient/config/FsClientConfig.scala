871d16caca57aa3066bfab3ebc5501864c0439c6
bartholomews <git@bartholomews.io>
2020-03-22 20:29:52 +0000

    `OAuthEnabled[OAuthVersion]` type alias

--- src/main/scala/fsclient/config/FsClientConfig.scala
+++ src/main/scala/fsclient/config/FsClientConfig.scala
@@ -1,5 +1,6 @@
 package fsclient.config
 
+import fsclient.entities.OAuthInfo.OAuthV1
 import fsclient.entities.OAuthVersion.V1
 import fsclient.entities._
 import org.http4s.client.oauth1.Consumer
@@ -11,7 +12,7 @@ object FsClientConfig {
 
   import pureconfig.generic.auto._
 
-  private def v1(consumerConfig: ConsumerConfig): FsClientConfig[OAuthEnabled[V1.type]] = {
+  private def v1(consumerConfig: ConsumerConfig): FsClientConfig[OAuthV1] = {
     val signer: Signer[V1.type] = V1.BasicSignature(Consumer(consumerConfig.key, consumerConfig.secret))
     new FsClientConfig(
       UserAgent(consumerConfig.appName, consumerConfig.appVersion, consumerConfig.appUrl),
@@ -19,15 +20,15 @@ object FsClientConfig {
     )
   }
 
-  def v1(userAgent: UserAgent, consumer: Consumer): FsClientConfig[OAuthEnabled[V1.type]] = {
+  def v1(userAgent: UserAgent, consumer: Consumer): FsClientConfig[OAuthV1] = {
     val signer: Signer[V1.type] = V1.BasicSignature(consumer)
     FsClientConfig(userAgent, OAuthEnabled(signer))
   }
 
-  def v1(): FsClientConfig[OAuthEnabled[V1.type]] =
+  def v1(): FsClientConfig[OAuthV1] =
     FsClientConfig.v1(ConfigSource.default.loadOrThrow[Config].consumer)
 
-  def v1(key: String): FsClientConfig[OAuthEnabled[V1.type]] = {
+  def v1(key: String): FsClientConfig[OAuthV1] = {
     implicit val customConfigReader: Derivation[ConfigReader[Config]] = Derivations.withCustomKey(key)
     FsClientConfig.v1(ConfigSource.default.loadOrThrow[Config].consumer)
   }
--- src/main/scala/fsclient/entities/OAuthInfo.scala
+++ src/main/scala/fsclient/entities/OAuthInfo.scala
@@ -4,3 +4,8 @@ sealed trait OAuthInfo
 
 case object OAuthDisabled extends OAuthInfo
 case class OAuthEnabled[V <: OAuthVersion](signer: Signer[V]) extends OAuthInfo
+
+object OAuthInfo {
+  type OAuthV1 = OAuthEnabled[OAuthVersion.V1.type]
+  type OAuthV2 = OAuthEnabled[OAuthVersion.V2.type]
+}
