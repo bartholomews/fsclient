7929c14d32385db84d75c4569674c91f86e3bff4
bartholomews <git@bartholomews.io>
2020-03-29 18:38:34 +0100

    `type StaticTokenV1`

--- src/main/scala/fsclient/entities/OAuthInfo.scala
+++ src/main/scala/fsclient/entities/OAuthInfo.scala
@@ -7,5 +7,6 @@ case class OAuthEnabled[V <: OAuthVersion](signer: Signer[V]) extends OAuthInfo
 
 object OAuthInfo {
   type OAuthV1 = OAuthEnabled[OAuthVersion.V1.type]
+  type StaticTokenV1 = OAuthEnabled[OAuthVersion.V1.AccessToken]
   type OAuthV2 = OAuthEnabled[OAuthVersion.V2.type]
 }
