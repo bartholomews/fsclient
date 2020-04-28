58f282c0893b2946ea958b32ddf859f0fd16e508
bartholomews <git@bartholomews.io>
2020-04-28 20:57:12 +0100

    OAuthV1AuthorizationFramework.SignerType

--- src/main/scala/io/bartholomews/fsclient/entities/oauth/v1/OAuthV1AuthorizationFramework.scala
+++ src/main/scala/io/bartholomews/fsclient/entities/oauth/v1/OAuthV1AuthorizationFramework.scala
@@ -5,6 +5,13 @@ import io.bartholomews.fsclient.requests.FsAuthRequest
 import org.http4s.Uri
 
 object OAuthV1AuthorizationFramework {
+
+  sealed trait SignerType
+  object SignerType {
+    case object BasicSignature extends SignerType
+    case object TokenSignature extends SignerType
+  }
+
   // https://tools.ietf.org/html/rfc5849#section-2.2
   case class AccessTokenRequest(uri: Uri) extends FsAuthRequest.Post[Nothing, String, AccessTokenCredentials] {
     final override val body: Option[Nothing] = None
