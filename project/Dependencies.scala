aaaa500f62d7c0acdf4a4f1c013abf8e2a7ef399
Scala Steward <me@scala-steward.org>
2021-09-17 22:28:51 +0200

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Versions {
   // https://github.com/pureconfig/pureconfig/releases
   val pureConfig = "0.16.0"
   // https://github.com/scalatest/scalatest/releases
-  val scalaTest = "3.2.9"
+  val scalaTest = "3.2.10"
   // https://github.com/softwaremill/sttp/releases
   val sttp = "3.3.14"
   // https://github.com/tomakehurst/wiremock/releases
